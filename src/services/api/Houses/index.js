75d6cb7eb3c075b834bc5d97ed736dbe100c171d
Diego Mendoza <dmendoza@inflexionsoftware.com>
2018-03-27 22:06:54 -0600

    Le agregue la función encodeURIComponent a los parametros que se envían por GET al servicio, para evitar problemas de encode.

--- src/services/api/Houses/index.js
+++ src/services/api/Houses/index.js
@@ -2,9 +2,9 @@ import apiConfig from '../../config';
 
 //Obtienen las casas publicadas con base a ciertos filtros
 export const getPublishedHouses = (filters) => {
-  let propertyFiler = filters.houseProperty ? filters.houseProperty : '';
-  let operationFiler = filters.houseOperation ? filters.houseOperation : '';
-  let keywordFiler = filters.houseKeyword ? filters.houseKeyword : '';
+  let propertyFiler = filters.houseProperty ? encodeURIComponent(filters.houseProperty) : '';
+  let operationFiler = filters.houseOperation ? encodeURIComponent(filters.houseOperation) : '';
+  let keywordFiler = filters.houseKeyword ? encodeURIComponent(filters.houseKeyword) : '';
   return fetch(`${apiConfig.apiurl}/published-houses?max=0&min=0&operation=${operationFiler}&property=${propertyFiler}&search=${keywordFiler}`)
         .then((response) => {
             var users = response.json();
