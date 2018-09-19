2d90ee67f7eca18798896982c693a16bd3c17feb
Diego Mendoza <dmendoza@inflexionsoftware.com>
2018-09-18 22:27:24 -0500

    Pruebas del formato de fecha que se debe utilizar para implementar la paginación en el backend.

--- src/services/api/Houses/index.js
+++ src/services/api/Houses/index.js
@@ -7,7 +7,9 @@ export const getPublishedHousesV2 = (filters) => {
   let keywordFiler = filters.selectedKeyword ? encodeURIComponent(filters.selectedKeyword) : '';
   let minPrice = filters.selectedPriceRange.min;
   let maxPrice = filters.selectedPriceRange.max;
+  let dateOfLastHouse = new Date(2017,4,1);
+  dateOfLastHouse = encodeURIComponent(dateOfLastHouse.toISOString());
 
-  return fetch(`http://localhost:3000/api/published-houses?min=${minPrice}&max=${maxPrice}&operation=${operationFilter}&property=${propertyFilter}&search=${keywordFiler}`)
+  return fetch(`http://localhost:3000/api/published-houses?min=${minPrice}&max=${maxPrice}&operation=${operationFilter}&property=${propertyFilter}&search=${keywordFiler}&itemLastDate=${dateOfLastHouse}&pagDirection=r`)
     .then(response => response.json());
 };
