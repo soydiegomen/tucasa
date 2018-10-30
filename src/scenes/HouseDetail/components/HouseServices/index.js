18923d4b0188438a51ebbfa30212d6f946554b41
Diego Mendoza <dmendoza@inflexionsoftware.com>
2018-10-29 22:54:52 -0600

    Conecte el control de los servicios para que se llene con la información del API

--- src/scenes/HouseDetail/components/HouseServices/index.js
+++ src/scenes/HouseDetail/components/HouseServices/index.js
@@ -1,54 +1,61 @@
 import React, { Component } from 'react';
+//Redux
+import { connect } from 'react-redux';
 
 class HouseServices extends Component {
 
+  renderServiceitem (service, i){
+    let classService = ""
+    switch (service) {
+      case "Internet":
+        classService = "fa-wifi";
+        break;
+      case "Telefono":
+        classService = "fa-phone";
+        break;
+      case "Cocina":
+        classService = "fa-cutlery";
+        break;
+      case "Jardin":
+        classService = "fa-paw";
+        break;
+      case "Mascotas":
+        classService = "fa-paw";
+        break;
+      case "SeguridadPrivada":
+        classService = "fa-taxi";
+        break;
+    }
+
+    return (
+      <div className="col-md-4" key={i}>
+        <div className="service-item">
+          <i className={'fa ' + classService} aria-hidden="true"></i>
+          {service}
+        </div>
+      </div>);
+  }
+
   render() {
+      const services = this.props.house.services;
+      const thisObj = this;
       return (
         <div className="house-details">
           <h4>Servicios</h4>
           <div className="services-list">
             <div className="row">
-              <div className="col-md-4">
-                <div className="service-item">
-                  <i className="fa fa-wifi" aria-hidden="true"></i>
-                  Internet
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="service-item">
-                  <i className="fa fa-phone" aria-hidden="true"></i>
-                  Linea telefónica
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="service-item">
-                  <i className="fa fa-cutlery" aria-hidden="true"></i>
-                  Cocina
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="service-item">
-                  <i className="fa fa-paw" aria-hidden="true"></i>
-                  Admite mascotas
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="service-item">
-                  <i className="fa fa-child" aria-hidden="true"></i>
-                  Área de juegos infantiles
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="service-item">
-                  <i className="fa fa-taxi" aria-hidden="true"></i>
-                  Seguridad privada
-                </div>
-              </div>
+            {services.map(function(service, i){
+               return thisObj.renderServiceitem(service, i);
+             })}
             </div>
           </div>
         </div>
       );
     }
 }
+function mapStateToProps(state) {
+	const { house } = state;
+  return { house };
+}
 
-export default HouseServices;
+export default connect(mapStateToProps)(HouseServices);
