f628b0e4a5cbf82b8bd9500c72850ae80656a7f6
Diego Mendoza <dmendoza@inflexionsoftware.com>
2019-01-29 22:43:46 -0600

    Se agrego la primera versión del slider en la pantalla de detalles de la casa

--- src/scenes/HouseDetail/components/HouseCarousel/index.js
+++ src/scenes/HouseDetail/components/HouseCarousel/index.js
@@ -2,25 +2,40 @@ import React, { Component } from 'react';
 //Redux
 import { connect } from 'react-redux';
 //components
+import { Slide } from 'react-slideshow-image';
 import { buildHouseImage } from 'services/utilities/ImageHelper';
 
-class HouseCarousel extends Component {
+const slideImages = [
+  'http://public.tucasa.com:3300/static/uploads/2017/10/12/min-1507865283058-dies-a.jpg',
+  'http://public.tucasa.com:3300/static/uploads/2017/10/12/min-1507865283058-dies-a.jpg'
+];
 
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true
+}
+
+class HouseCarousel extends Component {
   render() {
     const filesData = this.props.house.filesData;
     return (
-        <div className="house-images">
-          <div className="house-image-content">
-            {
-              (filesData.length === 0) &&
-              <div className="alert alert-secondary">
-                Esta propiedad no tiene una imágen establecida
+        <div>
+          <Slide {...properties}>
+            <div className="each-slide">
+              <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+                <span>Slide 1</span>
               </div>
-            }
-            {
-              (filesData.length > 0) && buildHouseImage(filesData[0].fileUrl)
-            }
-          </div>
+            </div>
+            <div className="each-slide">
+              <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+                <span>Slide 2</span>
+              </div>
+            </div>
+          </Slide>
+
         </div>
     );
   }
@@ -32,3 +47,18 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(HouseCarousel);
+/*
+<div className="house-images">
+  <div className="house-image-content">
+    {
+      (filesData.length === 0) &&
+      <div className="alert alert-secondary">
+        Esta propiedad no tiene una imágen establecida
+      </div>
+    }
+    {
+      (filesData.length > 0) && buildHouseImage(filesData[0].fileUrl)
+    }
+  </div>
+</div>
+*/
