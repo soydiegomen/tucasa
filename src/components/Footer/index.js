c9c8246fefbe7950ddcd51b2d295715800b075fc
Diego Mendoza <dmendoza@inflexionsoftware.com>
2019-02-26 22:51:17 -0600

    Agregue la primer versión del diseño del footer y mejoras menores en el header.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logoFoot from './logo_foot.png';
 
 class Footer extends Component {
   render() {
       return (
-        <footer>
-          <div className="text-right font-weight-light">
-            <a href="www.tucasa.com" alt="Tu casa">
-              www.tucasa.com
-            </a>
+        <footer className="tucasa-footer">
+          <div className="container">
+            <div className="row">
+              <div className="col-4">
+                <img src={logoFoot}  alt="" />
+                <div className="footer-content">
+                  This is the perfect four bedroom family home on an idyllic block, in one of Chicagos nicest suburbs.
+                </div>
+                <div className="foot-social-links">
+                  <ul className="list-inline">
+                    <li className="list-inline-item">
+                      <Link to={"/"}>
+                        <i className="fa fa-facebook" aria-hidden="true"></i>
+                      </Link>
+                    </li>
+                    <li className="list-inline-item">
+                      <Link to={"/"}>
+                        <i className="fa fa-instagram" aria-hidden="true"></i>
+                      </Link>
+                    </li>
+                    <li className="list-inline-item">
+                      <Link to={"/"}>
+                        <i className="fa fa-twitter" aria-hidden="true"></i>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-4">
+                <span className="footer-title">Useful Links</span>
+                <div className="footer-content">
+                  <ul className="list-unstyled">
+                    <li>
+                      <Link to={"/"}>Inicio</Link>
+                    </li>
+                    <li>
+                      <Link to={"/"}>Destacados</Link>
+                    </li>
+                    <li>
+                      <Link to={"/"}>Blog</Link>
+                    </li>
+                    <li>
+                      <Link to={"/"}>Avisos oportunos</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-4">
+                <span className="footer-title">Twitter Feed</span>
+                <div className="footer-content">
+                @TrxDoe RT @envato: One subscription, 5000+ assets & unlimited downloads; Elements is out of beta!
+                </div>
+              </div>
+            </div>
           </div>
         </footer>
       );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
 import { Navbar, Form, FormControl, Button, NavDropdown, Nav  } from 'react-bootstrap';
-import brandlogo from './taza-chai.png';
+import brandlogo from './logo-house.png';
 
 class Header extends Component {
   render() {
       return (
-        <header>            
-            <Navbar bg="light" expand="lg">
-              <Navbar.Brand href="#home">
-                <img src={brandlogo}  alt="" />
-                TuCasaEnJilo.com
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                  <Nav.Link href="#home">Inicio</Nav.Link>
-                  <Nav.Link href="#link">Salones de fiesta</Nav.Link>
-                  <Nav.Link href="#link">Locales</Nav.Link>
-                  <Nav.Link href="#link">Terrenos</Nav.Link>
-                </Nav>
-                <Form inline>
-                  <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-              </Navbar.Collapse>
-            </Navbar>
+        <header>
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <Navbar bg="light" expand="lg">
+                  <Navbar.Brand href="#home">
+                    <img src={brandlogo}  alt="" />
+                  </Navbar.Brand>
+                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                      <Nav.Link href="#home">Inicio</Nav.Link>
+                      <Nav.Link href="#link">Destacados</Nav.Link>
+                      <Nav.Link href="#link">Blog</Nav.Link>
+                      <Nav.Link href="#link">Avisos oportunos</Nav.Link>
+                    </Nav>
+                    <Form inline>
+                      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                      <Button variant="outline-success">Search</Button>
+                    </Form>
+                  </Navbar.Collapse>
+                </Navbar>
+              </div>
+            </div>
+          </div>
         </header>
       );
     }
